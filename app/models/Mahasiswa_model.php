55b3f33f2c57e21d9e18c8cf85bfff709afdd507
puturangga21 <puturangga21@gmail.com>
2022-09-11 22:11:16 +0800

    first commit

---
+++ app/models/Mahasiswa_model.php
@@ -0,0 +1,42 @@
+<?php 
+class Mahasiswa_model  {
+
+   private $table = 'mahasiswa';
+   private $db;
+
+   public function __construct()
+   {
+      $this->db = new Database;
+   }
+
+   public function getAllmahasiswa () {
+      $this->db->query('SELECT * FROM ' . $this->table);
+      return $this->db->resultSet();
+   }
+
+   public  function  getMahasiswaById($id)
+   {
+      $this->db->query('SELECT * FROM '  . $this->table . ' WHERE id=:id ');
+      $this->db->bind('id', $id);
+      return  $this->db->single();
+   }
+
+   public function tambahDataMahasiswa($data){
+      $query = "INSERT INTO mahasiswa
+      values ('', :nama, :nim, :email, :jurusan)";
+
+      $this->db->query($query);
+      $this->db->bind('nama', $data['nama']);
+      $this->db->bind('nim', $data['nim']);
+      $this->db->bind('email', $data['email']);
+      $this->db->bind('jurusan', $data['jurusan']);
+
+      $this->db->execute();
+
+      return $this->db->rowCount();
+      }
+
+
+}
+
+?>
---
+++ app/views/about/index.php
@@ -0,0 +1,6 @@
+<div class="container">
+   <h1 class="mt-4">Halaman About</h1>
+   <img src="<?= BASEURL; ?>/img/foto1.jpg" alt="Putu Rangga" width="200" class="rounded-circle shadow">
+   <p>Halo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, pekerjaan saya <?= $data['pekerjaan']; ?>
+   </p>
+</div>
